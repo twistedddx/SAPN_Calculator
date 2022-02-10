fca630873e79f3f4b8db2a610e29b34d7be10436
twistedddx <kickbits@gmail.com>
2022-02-10 22:44:05 +1030

    Add project files.

---
+++ SAPN_Calculator/Main_Window.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace SAPN_Calculator
+{
+    partial class SAPN_Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox_inputFile = new System.Windows.Forms.GroupBox();
+            this.textBox_inputFile = new System.Windows.Forms.TextBox();
+            this.button_browse = new System.Windows.Forms.Button();
+            this.button_run = new System.Windows.Forms.Button();
+            this.groupBox_options = new System.Windows.Forms.GroupBox();
+            this.comboBox_loglevel = new System.Windows.Forms.ComboBox();
+            this.comboBox_schedule = new System.Windows.Forms.ComboBox();
+            this.comboBox_duration = new System.Windows.Forms.ComboBox();
+            this.groupBox_output = new System.Windows.Forms.GroupBox();
+            this.textBox_output = new System.Windows.Forms.TextBox();
+            this.groupBox_inputFile.SuspendLayout();
+            this.groupBox_options.SuspendLayout();
+            this.groupBox_output.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox_inputFile
+            // 
+            this.groupBox_inputFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox_inputFile.Controls.Add(this.textBox_inputFile);
+            this.groupBox_inputFile.Controls.Add(this.button_browse);
+            this.groupBox_inputFile.Controls.Add(this.button_run);
+            this.groupBox_inputFile.Location = new System.Drawing.Point(20, 10);
+            this.groupBox_inputFile.Name = "groupBox_inputFile";
+            this.groupBox_inputFile.Size = new System.Drawing.Size(945, 50);
+            this.groupBox_inputFile.TabIndex = 0;
+            this.groupBox_inputFile.TabStop = false;
+            this.groupBox_inputFile.Text = "Input File";
+            // 
+            // textBox_inputFile
+            // 
+            this.textBox_inputFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_inputFile.Location = new System.Drawing.Point(15, 18);
+            this.textBox_inputFile.Name = "textBox_inputFile";
+            this.textBox_inputFile.Size = new System.Drawing.Size(725, 20);
+            this.textBox_inputFile.TabIndex = 0;
+            this.textBox_inputFile.Text = "C:\\_SAPN_DETAILED.csv";
+            // 
+            // button_browse
+            // 
+            this.button_browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_browse.Location = new System.Drawing.Point(755, 18);
+            this.button_browse.Name = "button_browse";
+            this.button_browse.Size = new System.Drawing.Size(80, 21);
+            this.button_browse.TabIndex = 1;
+            this.button_browse.Text = "Browse";
+            this.button_browse.UseVisualStyleBackColor = true;
+            this.button_browse.Click += new System.EventHandler(this.button_browse_Click);
+            // 
+            // button_run
+            // 
+            this.button_run.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_run.Location = new System.Drawing.Point(850, 18);
+            this.button_run.Name = "button_run";
+            this.button_run.Size = new System.Drawing.Size(80, 21);
+            this.button_run.TabIndex = 2;
+            this.button_run.Text = "Run";
+            this.button_run.UseVisualStyleBackColor = true;
+            this.button_run.Click += new System.EventHandler(this.button_run_Click);
+            // 
+            // groupBox_options
+            // 
+            this.groupBox_options.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox_options.Controls.Add(this.comboBox_loglevel);
+            this.groupBox_options.Controls.Add(this.comboBox_schedule);
+            this.groupBox_options.Controls.Add(this.comboBox_duration);
+            this.groupBox_options.Location = new System.Drawing.Point(20, 65);
+            this.groupBox_options.Name = "groupBox_options";
+            this.groupBox_options.Size = new System.Drawing.Size(945, 50);
+            this.groupBox_options.TabIndex = 1;
+            this.groupBox_options.TabStop = false;
+            this.groupBox_options.Text = "Options";
+            // 
+            // comboBox_loglevel
+            // 
+            this.comboBox_loglevel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox_loglevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_loglevel.FormattingEnabled = true;
+            this.comboBox_loglevel.Location = new System.Drawing.Point(780, 18);
+            this.comboBox_loglevel.Name = "comboBox_loglevel";
+            this.comboBox_loglevel.Size = new System.Drawing.Size(150, 21);
+            this.comboBox_loglevel.TabIndex = 2;
+            // 
+            // comboBox_schedule
+            // 
+            this.comboBox_schedule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_schedule.FormattingEnabled = true;
+            this.comboBox_schedule.Location = new System.Drawing.Point(185, 18);
+            this.comboBox_schedule.Name = "comboBox_schedule";
+            this.comboBox_schedule.Size = new System.Drawing.Size(150, 21);
+            this.comboBox_schedule.TabIndex = 1;
+            // 
+            // comboBox_duration
+            // 
+            this.comboBox_duration.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_duration.FormattingEnabled = true;
+            this.comboBox_duration.Location = new System.Drawing.Point(15, 18);
+            this.comboBox_duration.Name = "comboBox_duration";
+            this.comboBox_duration.Size = new System.Drawing.Size(150, 21);
+            this.comboBox_duration.TabIndex = 0;
+            // 
+            // groupBox_output
+            // 
+            this.groupBox_output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox_output.Controls.Add(this.textBox_output);
+            this.groupBox_output.Location = new System.Drawing.Point(20, 120);
+            this.groupBox_output.Name = "groupBox_output";
+            this.groupBox_output.Size = new System.Drawing.Size(945, 505);
+            this.groupBox_output.TabIndex = 2;
+            this.groupBox_output.TabStop = false;
+            this.groupBox_output.Text = "Output";
+            // 
+            // textBox_output
+            // 
+            this.textBox_output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_output.Location = new System.Drawing.Point(15, 20);
+            this.textBox_output.Multiline = true;
+            this.textBox_output.Name = "textBox_output";
+            this.textBox_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_output.Size = new System.Drawing.Size(915, 470);
+            this.textBox_output.TabIndex = 0;
+            this.textBox_output.Text = "Load a detailed summary and click run.";
+            // 
+            // SAPN_Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 641);
+            this.Controls.Add(this.groupBox_options);
+            this.Controls.Add(this.groupBox_output);
+            this.Controls.Add(this.groupBox_inputFile);
+            this.MinimumSize = new System.Drawing.Size(570, 300);
+            this.Name = "SAPN_Calculator";
+            this.Text = "SAPN Calculator";
+            this.groupBox_inputFile.ResumeLayout(false);
+            this.groupBox_inputFile.PerformLayout();
+            this.groupBox_options.ResumeLayout(false);
+            this.groupBox_output.ResumeLayout(false);
+            this.groupBox_output.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_inputFile;
+        private System.Windows.Forms.TextBox textBox_inputFile;
+        private System.Windows.Forms.Button button_browse;
+        private System.Windows.Forms.Button button_run;
+
+        private System.Windows.Forms.GroupBox groupBox_options;
+        private System.Windows.Forms.ComboBox comboBox_schedule;
+        private System.Windows.Forms.ComboBox comboBox_duration;
+        private System.Windows.Forms.ComboBox comboBox_loglevel;
+
+        private System.Windows.Forms.GroupBox groupBox_output;
+        private System.Windows.Forms.TextBox textBox_output;
+    }
+}
+
---
+++ SAPN_Calculator/Main_Window.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+enum EnergyClass
+{
+    Import,
+    Export,
+    ControlledLoad
+}
+
+enum EnergyBracket
+{
+    Peak,
+    Offpeak,
+    Shoulder
+}
+
+enum ReportDuration
+{
+    Hourly,
+    Daily,
+    Monthly,
+    Seasonly,
+    Yearly
+}
+
+enum ReportSchedule
+{
+    Peak,
+    TOU,
+    DayNight
+}
+
+enum LogLevel
+{
+    Info,
+    Warn,
+    Debug
+}
+
+namespace SAPN_Calculator
+{
+    public partial class SAPN_Calculator : Form
+    {
+        //0=peak,1=offpeak,2=shoulder
+        //Hour starting             0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23
+        readonly int[] SchedulePeak      = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        readonly int[] ScheduleTOU       = { 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        readonly int[] ScheduleDayNight  = { 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 };
+
+
+        static string filePath = string.Empty;
+
+        static int meterNum = 0;
+
+        public SAPN_Calculator()
+        {
+            InitializeComponent();
+
+            comboBox_duration.DataSource = Enum.GetValues(typeof(ReportDuration));
+            comboBox_duration.SelectedItem = ReportDuration.Monthly;
+
+            comboBox_schedule.DataSource = Enum.GetValues(typeof(ReportSchedule));
+            comboBox_schedule.SelectedItem = ReportSchedule.TOU;
+
+            comboBox_loglevel.DataSource = Enum.GetValues(typeof(LogLevel));
+            comboBox_loglevel.SelectedItem = LogLevel.Info;
+        }
+
+        public string[] WriteSafeReadAllLines(String path)
+        {
+            using (var csv = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var sr = new StreamReader(csv))
+            {
+                List<string> file = new List<string>();
+                while (!sr.EndOfStream)
+                {
+                    file.Add(sr.ReadLine());
+                }
+
+                return file.ToArray();
+            }
+        }
+
+        public bool isNew(int current, ref int previous)
+        {
+            bool isNew;
+            if (previous == 0)
+            {
+                isNew = false;
+            }
+            else if (current != previous)
+            {
+                isNew = true;
+            }
+            else
+            {
+                isNew = false;
+            }
+            previous = current;
+            return isNew;
+        }
+
+
+private void button_browse_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Open Detailed Summary CSV file";
+                openFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            textBox_inputFile.Text = filePath;
+        }
+
+        private void button_run_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(filePath))
+            {
+                textBox_output.Text = "Cannot open file. Try loading another detailed summary and press run.";
+            }
+            else
+            {
+                //get options
+                ReportDuration? reportDuration = comboBox_duration.SelectedValue as ReportDuration?;
+                ReportSchedule? reportSchedule = comboBox_schedule.SelectedValue as ReportSchedule?;
+                LogLevel? logLevel = comboBox_loglevel.SelectedValue as LogLevel?;
+
+                int[] currentSchedule;
+                if (reportSchedule == ReportSchedule.TOU)
+                {
+                    currentSchedule = ScheduleTOU;
+                }
+                else if (reportSchedule == ReportSchedule.DayNight)
+                {
+                    currentSchedule = ScheduleDayNight;
+                }
+                else
+                {
+                    currentSchedule = SchedulePeak;
+                }
+
+                //read the csv
+                string[] lines = WriteSafeReadAllLines(filePath);
+                var lineCount = lines.Count();
+
+                if (lineCount > 0)
+                {
+                    string date = "";
+                    EnergyClass energyClass = EnergyClass.Import;
+                    int interval = 0;
+
+                    bool isNewYear, isNewMonth, isNewDay;
+                    int year;
+                    int month;
+                    int day;
+                    int previousYear = 0;
+                    int previousMonth = 0;
+                    int previousDay = 0;
+                    bool dataPrinted = false;
+
+                    float[] dayData = new float[3];
+                    float[] monthData = new float[3];
+                    float[] yearData = new float[3];
+
+                    EnergyClass previousEnergyClass = EnergyClass.Import;
+
+                    textBox_output.Text = "";
+
+                    for (int linenum = 0; linenum < lineCount; linenum++)
+                    {
+                        string[] columns = lines[linenum].Split(',');
+
+                        if (columns[0] == "200") //meta
+                        {
+
+                            meterNum = int.Parse(columns[1]);
+
+                            if (columns[3] == "E1")
+                            {
+                                energyClass = EnergyClass.Import;
+                            }
+                            else if (columns[3] == "B1")
+                            {
+                                energyClass = EnergyClass.Export;
+                            }
+                            else if (columns[3] == "Q1")
+                            {
+                                energyClass = EnergyClass.ControlledLoad;
+                            }
+                            else
+                            {
+                                if (logLevel >= LogLevel.Warn)
+                                {
+                                    textBox_output.AppendText("Unknown EnergyClass " + columns[3] + " assuming Import\r\n");
+                                }
+                                energyClass = EnergyClass.Import;
+                            }
+
+                            //if energyClass changes create new dates for isNew
+                            if (energyClass != previousEnergyClass)
+                            {
+                                previousYear = previousMonth = previousDay = 1;
+                            }
+                            previousEnergyClass = energyClass;
+
+
+                            interval = int.Parse(columns[8]);
+
+                            if (logLevel >= LogLevel.Debug)
+                            {
+                                textBox_output.AppendText("Meter: " + meterNum + "  EnergyClass: " + energyClass.ToString() + "  Interval: " + interval + "\r\n");
+                            }
+                        }
+                        else if (columns[0] == "300") //data
+                        {
+                            dataPrinted = false;
+
+                            date = columns[1];
+
+                            year = int.Parse(date.Substring(0, 4));
+                            month = int.Parse(date.Substring(4, 2));
+                            day = int.Parse(date.Substring(6, 2));
+
+                            isNewYear = isNew(year, ref previousYear);
+                            isNewMonth = isNew(month, ref previousMonth);
+                            //isNewDay = isNew(day, ref previousDay); //it is always a new day
+
+                            var intervalsPerHour = 60 / interval;
+                            var intervalsPerDay = 24 * intervalsPerHour;
+
+                            float[] hourData = new float[24];
+
+                            if (reportDuration >= ReportDuration.Hourly)
+                            {
+                                int hour = 0;
+                                for (int currentInterval = 0; currentInterval < intervalsPerDay; currentInterval++)
+                                {
+                                    for (int x = 0; x < intervalsPerHour; x++)
+                                    {
+                                        hourData[hour] += float.Parse(columns[currentInterval + 2]); //+2 as [0] is 300, [1] is date
+                                        currentInterval++;
+                                    }
+                                    hour++;
+                                    currentInterval--; //correction as we are about to do one too many ++
+                                }
+                                if (reportDuration == ReportDuration.Hourly)
+                                {
+                                    textBox_output.AppendText(date + "," + energyClass.ToString() + "," + string.Join(",", hourData) + "\r\n");
+                                    dataPrinted = true;
+                                }
+                            }
+
+                            if (reportDuration >= ReportDuration.Daily)
+                            {
+                                Array.Clear(dayData, 0, dayData.Count());
+                                for (int x = 0; x < 24; x++)
+                                {
+                                    //ignore schedule for export and set all data to Peak?
+                                    if (energyClass == EnergyClass.Export)
+                                    {
+                                        dayData[(int)EnergyBracket.Peak] += hourData[x];
+                                    }
+                                    else
+                                    {
+                                        dayData[currentSchedule[x]] += hourData[x];
+                                    }
+                                }
+                                if (reportDuration == ReportDuration.Daily)
+                                {
+                                    textBox_output.AppendText(date + "," + energyClass.ToString() + "," + string.Join(",", dayData) + "\r\n");
+                                    dataPrinted = true;
+                                }
+                            }
+
+                            if (reportDuration >= ReportDuration.Monthly)
+                            {
+                                if (isNewMonth)
+                                {
+                                    if (reportDuration == ReportDuration.Monthly)
+                                    {
+                                        textBox_output.AppendText(date + "," + energyClass.ToString() + "," + string.Join(",", monthData) + "\r\n");
+                                        dataPrinted = true;
+                                    }
+                                    Array.Clear(monthData, 0, monthData.Count());
+                                }
+
+
+                                for (int x = 0; x < dayData.Count(); x++)
+                                {
+                                    monthData[x] += dayData[x];
+                                }
+                            }
+
+                            if (reportDuration >= ReportDuration.Yearly)
+                            {
+                                if (isNewYear)
+                                {
+                                    if (reportDuration == ReportDuration.Yearly)
+                                    {
+                                        textBox_output.AppendText(date + "," + energyClass.ToString() + "," + string.Join(",", yearData) + "\r\n");
+                                        dataPrinted = true;
+                                    }
+                                    Array.Clear(yearData, 0, yearData.Count());
+                                }
+
+                                if (isNewMonth)
+                                {
+                                    for (int x = 0; x < monthData.Count(); x++)
+                                    {
+                                        yearData[x] += monthData[x];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    //last entry did not print as there was not a new year/month trigger, force print here
+                    if (dataPrinted == false)
+                    {
+                        if (reportDuration == ReportDuration.Yearly)
+                        {
+                            //last month also did not get added
+                            for (int x = 0; x < monthData.Count(); x++)
+                            {
+                                yearData[x] += monthData[x];
+                            }
+
+                            textBox_output.AppendText(date + "," + energyClass.ToString() + "," + string.Join(",", yearData) + "\r\n");
+                            dataPrinted = true;
+                        }
+
+                        if (reportDuration == ReportDuration.Monthly)
+                        {
+                            textBox_output.AppendText(date + "," + energyClass.ToString() + "," + string.Join(",", monthData) + "\r\n");
+                            dataPrinted = true;
+                        }
+                    }
+                }
+                else // lineCount == 0
+                {
+                    textBox_output.Text = "No data? Try loading another detailed summary and press run.";
+                }
+            }
+        }
+    }
+}
